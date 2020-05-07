0d4f3439f21449f3f794096252f7d29385904c39
Dale Newman <dalenewman@gmail.com>
2020-05-07 16:52:24 -0400

    allow profiled connection factory to be passed in

--- nuget/myget.bat
+++ nuget/myget.bat
@@ -1,8 +1,8 @@
-REM nuget pack Transformalize.Provider.MySql.nuspec -OutputDirectory "c:\temp\modules"
-REM nuget pack Transformalize.Provider.MySql.Autofac.nuspec -OutputDirectory "c:\temp\modules"
-REM 
-REM nuget push "c:\temp\modules\Transformalize.Provider.MySql.0.7.6-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.MySql.Autofac.0.7.6-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget pack Transformalize.Provider.MySql.nuspec -OutputDirectory "c:\temp\modules"
+nuget pack Transformalize.Provider.MySql.Autofac.nuspec -OutputDirectory "c:\temp\modules"
+
+REM nuget push "c:\temp\modules\Transformalize.Provider.MySql.0.7.7-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.MySql.Autofac.0.7.7-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
 
 
