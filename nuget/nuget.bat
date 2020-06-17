123060b6a56f26cf458e86a0a4430f6254bd1555
Dale Newman <dalenewman@gmail.com>
2020-06-17 00:44:12 -0400

    Updated for Transformalize 0.8.1

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,8 +1,8 @@
 REM nuget pack Transformalize.Provider.MySql.nuspec -OutputDirectory "c:\temp\modules"
 REM nuget pack Transformalize.Provider.MySql.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.MySql.0.7.6-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.MySql.Autofac.0.7.6-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.MySql.0.8.1-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.MySql.Autofac.0.8.1-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
 
