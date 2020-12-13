9ff9263d844fcc2366fd3f7fad426440eae01a7b
Dale Newman <dalenewman@gmail.com>
2020-12-13 15:01:33 -0500

    Nuget Updates

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,8 +1,8 @@
 nuget pack Transformalize.Provider.MySql.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.MySql.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.MySql.0.8.15-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.MySql.Autofac.0.8.15-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.MySql.0.8.29-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.MySql.Autofac.0.8.29-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
 
